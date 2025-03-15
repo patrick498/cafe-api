378786a934fcda25439ad7103c09373f93e58f4b
Patrick Pailhes <193191145+patrick498@users.noreply.github.com>
2025-03-15 14:17:07 +0900

    Created cafe seeds

--- app/models/cafe.rb
+++ app/models/cafe.rb
@@ -1,4 +1,4 @@
 class Cafe < ApplicationRecord
-  validates :title, presence: true, uniqueness: { scope: address }
+  validates :title, presence: true, uniqueness: { scope: :address }
   validates :address, presence: true
 end
